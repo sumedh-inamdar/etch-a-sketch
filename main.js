10f70c29b8339fc8c04cbb9a36161de306875cc7
Sumedh Inamdar <sinamdar@gmail.com>
2021-09-01 14:37:55 -0700

    Added border formatting, buttons, positioning

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const gridContainerNode = document.querySelector('.gridContainer');
+const clearGrid = document.querySelector('.clearGrid');
 
+//Create 16x16 grid + add hover effect
 for (let i=0; i < 16*16; i++) {
     const divNode = document.createElement('div');
     divNode.addEventListener("mouseover", () => {
@@ -7,5 +9,6 @@ for (let i=0; i < 16*16; i++) {
     })
     gridContainerNode.appendChild(divNode);
 }
+
 
 
